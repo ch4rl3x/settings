b20750e9babb3bbabb59f041670acac404cc8665
Alexander Karkossa <alexander.karkossa@googlemail.com>
2021-11-03 13:16:26 +0100

    refactor: add ReplaceWith marker interfaces

---
+++ settings-datastore/src/main/java/de/charlex/settings/SettingsDataStore.kt
@@ -0,0 +1,4 @@
+package de.charlex.settings
+
+@Deprecated("SettingsDataStore was moved to another package", replaceWith = ReplaceWith(expression = "SettingsDataStore", imports = ["de.charlex.settings.datastore.SettingsDataStore"]), DeprecationLevel.ERROR)
+interface SettingsDataStore
---
+++ settings-sharedpreferences/src/main/java/de/charlex/settings/Settings.kt
@@ -0,0 +1,4 @@
+package de.charlex.settings
+
+@Deprecated("Settings was moved to another package", replaceWith = ReplaceWith(expression = "Settings", imports = ["de.charlex.settings.sharedpreferences.Settings"]), DeprecationLevel.ERROR)
+interface Settings
