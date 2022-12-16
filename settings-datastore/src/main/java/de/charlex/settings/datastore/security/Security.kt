62f345d94cc22aa6ea411ba697ce0bd2d32db45e
Julian Kalinowski <Julian.Kalinowski@dataport.de>
2022-12-16 09:23:36 +0100

    feat: better exception when key is null

--- settings-datastore/src/main/java/de/charlex/settings/datastore/security/Security.kt
+++ settings-datastore/src/main/java/de/charlex/settings/datastore/security/Security.kt
@@ -2,5 +2,6 @@ package de.charlex.settings.datastore.security
 
 interface Security {
     fun encryptData(text: String): String
+    @Throws(KeyNotFoundException::class)
     fun decryptData(encrypted: String): String
 }
